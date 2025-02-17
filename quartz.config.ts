1513dbac0fb92f1d9d866d7cd0e238aedf15c4cf
yant0 <80298071+yant0@users.noreply.github.com.>
2025-02-17 23:02:29 +0700

    Reversed tags

--- quartz.config.ts
+++ quartz.config.ts
@@ -68,7 +68,7 @@ const config: QuartzConfig = {
         },
         keepBackground: false,
       }),
-      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: true }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
